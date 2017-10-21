66977ad89fc63d86905aea97559abf6947b0651a
Unknown <ecamps@gmail.com>
2017-10-21 22:16:40 +0200

    Update app icon + update signIn scripts

--- www/js/google-signin.js
+++ www/js/google-signin.js
@@ -1,24 +1,28 @@
 var googleUser = {};
-  var startApp = function() {
-    gapi.load('auth2', function(){
-      // Retrieve the singleton for the GoogleAuth library and set up the client.
-      auth2 = gapi.auth2.init({
-        client_id: '339637138263-9t7ll2udiuqncstdaqmirnk97qpp6huv.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-        // Request scopes in addition to 'profile' and 'email'
-        // scope: 'additional_scope'
-      });
-      attachSignin(document.getElementById('customBtn'));
+var startApp = function() {
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: '339637138263-9t7ll2udiuqncstdaqmirnk97qpp6huv.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      // scope: 'additional_scope'
     });
-  };
+    attachSignin(document.getElementById('customBtn'));
+  });
+};
 
-  function attachSignin(element) {
-    console.log(element.id);
-    auth2.attachClickHandler(element, {},
-        function(googleUser) {
-          document.getElementById('name').innerText = "Signed in: " +
-              googleUser.getBasicProfile().getName();
-        }, function(error) {
-          console.log(JSON.stringify(error, undefined, 2));
-        });
-  }
+function attachSignin(element) {
+  console.log(element.id);
+  auth2.attachClickHandler(element, {},
+      function(googleUser) {
+        document.getElementById('name').innerText = "Signed in: " +
+            googleUser.getBasicProfile().getName();
+      }, function(error) {
+        console.log(JSON.stringify(error, undefined, 2));
+      });
+}
+
+$(document).ready(function() {
+  startApp();
+});
